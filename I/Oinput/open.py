c643ad5e591f9f8f56eabfbfb1643e255419625c
aymendif <aimanedifaoui@gmail.com>
2023-11-05 17:35:26 +0100

    open and read file

---
+++ I/Oinput/open.py
@@ -0,0 +1,17 @@
+#open technique,
+names=[]
+for _ in range(3):
+     names.append(input("what is your name ")) #append =ajouter element dans la list "names"
+for name in sorted(names):  #sorted = triee les element dans la list en ordre alphabitique
+    print (f"hello , {name}")
+#for i in range(len(names)):
+ #    file =open("names.txt","a")
+  #   file.write(f"{i}-{names[i]}\n")
+#file.close()
+#for better code and to close the file automaticly is better to use this code 
+for i in range(len(names)):
+    with open("names.txt","a") as file :
+         file.write(f"hello,{names[i]}\n")
+# how to read a file 
+
+   
---
+++ I/Oinput/readfile.py
@@ -0,0 +1,4 @@
+with open("names.txt","r") as filereader : #r for read
+     lines=filereader.readlines()
+for line in lines:
+    print (line)
