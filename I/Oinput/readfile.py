0ca416c5e0022e79ae8931a5bad9f9c068841775
aymendif <aimanedifaoui@gmail.com>
2023-12-03 01:16:55 +0100

    ss

--- I/Oinput/readfile.py
+++ I/Oinput/readfile.py
@@ -2,8 +2,16 @@ noms =[]
 with open("names.txt","r") as filereader : #r for read
      lines=filereader.readlines()
 
-
+for line in lines:
+    print (line.rstrip()) #rstrip orgnize the output
+ #for line in filereader  other methode to do the ligne 5
+    #print (line.rstrip())
 with open("names.txt") as nfile:
      for line in sorted(nfile) :
          noms.append(line.rstrip())
 print (noms )
+with open("students.csv","r") as Sfile:  #use of csv file
+     for line in Sfile.readlines():
+          #row=line.rstrip().split(",") is not good way to store two values in list insted we use the next ligne
+          nom,prenom=line.rstrip().split(",")
+          print (f"{nom} {prenom}")
